344191f37eb60ee78bc6c1c388d0aa14227d1932
Tina <tinazamiri7@gmail.com>
2024-10-17 13:59:59 -0400

    complete tokenizer

--- rlox/src/main.rs
+++ rlox/src/main.rs
@@ -3,8 +3,29 @@ mod parse;
 
 use token::*;
 use parse::*;
+use std::fs::File;
+use std::io::prelude::*;
+use std::env;
 
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+    if args.len() < 2 {
+        println!("You SUCK at specifying file input");
+    }
+
+    let file = File::open(args[1].to_string());
+    let mut source = String::new();
+    
+    match file {
+        Ok(mut f) => {
+            let mut source = String::new();
+            f.read_to_string(&mut source).unwrap();
+            let tkns = tokenize(source);
+            println!("TKNS: {:?}",tkns);
+        },
+        Err(e) => panic!("File does not exist??: {e:?}"),
+    }
+
+    
 }
