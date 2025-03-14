a9a30e4ac0eab3b9bd4e9789eac182155250eb84
Liam Naddell <liamnprg@gmail.com>
2025-03-13 23:40:27 -0400

    [#33] Refactor compilation code into compile.rs
    * gvar2 is broken, but this can be fixed later
    * Add all code having to do with compiling rlox into it's own file
    * Add an AST traversal class for code clarity

--- src/bc.rs
+++ src/bc.rs
@@ -1,11 +1,9 @@
 use std::fmt;
-use crate::Program;
-use std::collections::HashMap;
 
 #[repr(u8)]
 #[derive(Clone,Copy,Ord,PartialOrd,PartialEq,Eq,Debug)]
 #[allow(non_camel_case_types)]
-enum Opcode {
+pub enum Opcode {
     OP_RETURN = 0,
     OP_CONSTANT,
     //discard top value on stack
@@ -44,7 +42,7 @@ enum Opcode {
 /*STOP*/}
 
 impl Opcode {
-    fn from_u8(mo: u8) -> Opcode {
+    pub fn from_u8(mo: u8) -> Opcode {
         if mo < (Opcode::OP_NONE as u8) {
             // this is safe because mo >=0 and  mo < OP_NONE, and mo has u8 size.
             return unsafe { std::mem::transmute(mo) }
@@ -107,14 +105,12 @@ fn is_falsey(v: &Value) -> bool {
 }
 
 pub struct Function {
-    //index into vm.funcs
-    pub chunk: Chunk,
-    //arity of the function.
+    pub chunk: usize,
     pub arity: usize,
 }
 impl fmt::Display for Function {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f,"arity:{}> function:\n{}",self.arity,self.chunk)
+        write!(f,"arity:{}>\n",self.arity)
     }
 }
 
@@ -128,195 +124,24 @@ struct Frame {
     sp: usize,
 }
 
-#[derive(Clone,Copy)]
-struct Decl {
-    //friendly reminder: no decl may have id 0
-    decl_id: u64,
-    scope: u8,
-}
-struct CTStack {
-    decls: [Decl;u8::MAX as usize],
-    //lexical scope depth
-    current_depth: u8,
-    tip: u8,
-}
-
-impl CTStack {
-    fn new() -> CTStack {
-        return CTStack {decls: [Decl {decl_id:0,scope:0};255], tip: 0, current_depth: 0};
-    }
-    fn push_frame(&mut self) {
-        self.current_depth += 1;
-    }
-    fn pop_frame(&mut self) {
-        assert!(self.current_depth != 0);
-        for i in (0..(self.tip as usize)).rev() {
-            assert!(self.decls[i].decl_id != 0);
-            if self.decls[i].scope != self.current_depth {
-                break;
-            }
-            self.decls[i].decl_id = 0;
-            assert!(self.tip != 0);
-            self.tip -= 1;
-        }
-        self.current_depth -= 1;
-    }
-    fn add_decl(&mut self, d:u64) -> u8 {
-        assert!(self.decls[self.tip as usize].decl_id == 0);
-        self.decls[self.tip as usize].decl_id = d;
-        self.decls[self.tip as usize].scope = self.current_depth;
-        let ret = self.tip;
-        self.tip += 1;
-        return ret;
-    }
-    fn get_stack_offset_of_decl(&self, d:u64) -> Option<(u8,Decl)> {
-        let end = self.tip as usize;
-        for i in (0..end).rev() {
-            if self.decls[i].decl_id == d {
-                return Some((i as u8,self.decls[i]));
-            }
-        }
-        return None;
-    }
-}
-
 pub struct VM {
     //Means of storing Function's and their bytecode.
     //Array indexes are "function id's".
     pub funcs: Vec<Function>,
+    pub chunks: Vec<Chunk>,
     pub stack: Vec<Value>,
     frames: Vec<Frame>,
-    //used for allocating ct_name_to_id slots
-    next_id: u64,
-    //A bijection of variable/function names and their associated id's
-    ct_name_to_id: HashMap<String,u64>,
-    //A mapping from variable id to index in the `globals` array
-    ct_global_id_to_index: HashMap<u64,usize>,
     pub globals: Vec<Value>,
-    ct_stack: CTStack,
 }
 
+use crate::compile::CompilePass;
 impl VM {
-    pub fn ct_push_scope(&mut self) {
-        self.ct_stack.push_frame();
-    }
-    pub fn ct_pop_scope(&mut self) {
-        self.ct_stack.pop_frame();
-    }
-    pub fn ct_add_decl(&mut self, d:u64) -> u8 {
-        return self.ct_stack.add_decl(d);
-    }
-    fn ct_get_stack_offset_of_decl(&self, d:u64) -> Option<(u8,Decl)> {
-        return self.ct_stack.get_stack_offset_of_decl(d);
-    }
-    pub fn ct_get_id_of_var(&mut self, name: &str) -> u64 {
-        let v = self.ct_name_to_id.get(name);
-        if let Some(id) = v {
-            return *id;
-        }
-
-        let id = self.next_id;
-        self.next_id+=1;
-
-        self.ct_name_to_id.insert(name.to_string(),id);
-        return id;
-    }
-
-    pub fn ct_add_function(&mut self, name: String, func: Function) {
-        self.funcs.push(func);
-        let id = self.ct_get_id_of_var(&name);
-        let ind = self.funcs.len() - 1;
-        self.ct_global_id_to_index.insert(id, ind);
-    }
-
-    pub fn ct_get_function_index(&mut self, name: &str) -> Option<usize> {
-        //TODO: THIS FUNCTION STINKS!!!!!!!
-        let id = self.ct_get_id_of_var(name);
-        let index = self.ct_get_global_index(id);
-        return index;
-    }
-
-    //emit a variable assignment, either to a global or local
-    pub fn emit_assign_var(&mut self, ch: &mut Chunk, var_name: &str) {
-        let id = self.ct_get_id_of_var(var_name);
-        let Some((ofs,decl)) = self.ct_get_stack_offset_of_decl(id) else {
-            assert!(false,"TODO: Error handling");
-            return;
-        };
-        if decl.scope == 0 {
-            let index = self.ct_get_global_index(id).expect("ICE");
-            ch.add_set_global(index);
-            //exprs must return something.
-            ch.add_get_global(index);
-            return;
-        }
-        ch.add_set_local(ofs);
-        ch.add_get_local(ofs);
-    }
-
-
-    pub fn ct_get_global_index(&self, id:u64) -> Option<usize> {
-        let v = self.ct_global_id_to_index.get(&id);
-        return v.copied();
-    }
-
-    pub fn emit_create_var(&mut self, ch: &mut Chunk, var_name: &str) {
-        let id = self.ct_get_id_of_var(var_name);
-        //NOTE: Technically not useful for global variables...
-        let ofs = self.ct_add_decl(id);
-        if self.ct_stack.current_depth == 0 {
-            //global var
-            let index = self.globals.len();
-            let old_value = self.ct_global_id_to_index.insert(id,index);
-            //TODO: Error handling.
-            assert!(old_value == None);
-            ch.add_set_global(index);
-        } else {
-            //local var
-            ch.add_set_local(ofs);
-        }
-    }
-
-    pub fn emit_get_var(&mut self, ch: &mut Chunk, var_name: &str) {
-        let id = self.ct_get_id_of_var(var_name);
-        let (ofs,decl) = self.ct_get_stack_offset_of_decl(id).expect("ICE: reference to id which does not exist");
-        if decl.scope == 0 {
-            let index = self.ct_get_global_index(id).expect("ICE");
-            ch.add_get_global(index);
-            return;
-        }
-        ch.add_get_local(ofs);
-    }
-
-    pub fn patch_jump(&mut self, ch: &mut Chunk, pos: usize) {
-        //jump to the END of the current function.
-        let offset = ch.code.len() - pos;
-
-        let offset = match u8::try_from(offset) {
-            Ok(offset) => offset,
-            Err(_) => {
-                panic!("jump too big"); 
-            }
-        };
-
-        let opc = ch.code[pos -1];
-        let op = Opcode::from_u8(opc);
-        match op {
-            Opcode::OP_JUMP_IF_FALSE => ch.code[pos] = offset,
-            Opcode::OP_JUMP => ch.code[pos] = offset,
-            _ => panic!("cant do jump at this opcode"),
-        }
-    }
-
-    pub fn new() -> VM {
-        return VM {funcs: vec!(),
+    pub fn new(cp: CompilePass) -> VM {
+        return VM {funcs: cp.funcs,
+            chunks:cp.cnks,
             frames:vec!(),
             stack:vec!(),
-            ct_name_to_id:HashMap::new(),
-            ct_global_id_to_index: HashMap::new(),
-            globals:vec!(),
-            ct_stack: CTStack::new(),
-            next_id:1
+            globals: cp.globals,
         };
     }
     pub fn stack_len(&self) -> usize {
@@ -331,12 +156,6 @@ impl VM {
     pub fn pop_stack(&mut self) -> Value {
         return self.stack.pop().expect("ICE");
     }
-    pub fn display_bc(&self) {
-        for i in 0..self.funcs.len() {
-            let func = &self.funcs[i];
-            println!("<func #{} {}",i,func);
-        }
-    }
     fn get_fn(&self,opc:Opcode) -> FBinOp {
         use Opcode::*;
         return match opc {
@@ -346,12 +165,6 @@ impl VM {
             OP_DIVIDE => |x,y| {x / y},
             _ => { panic!("get fn failed compiler sucks")}
         };
-    }
-
-    pub fn compile(&mut self, ast: &Program) {
-        let mut cnk = Chunk::new();
-        ast.emit_bc(&mut cnk,self);
-        self.funcs.push(Function { chunk:cnk,arity:0});
     }
 
     fn current_frame(&self) -> Frame {
@@ -367,7 +180,7 @@ impl VM {
     }
 
     pub fn current_chunk(&self) -> &Chunk {
-        return &self.current_func().chunk;
+        return &self.chunks[self.current_func().chunk];
     }
 
     pub fn current_code(&self) -> &Vec<u8> {
@@ -384,7 +197,7 @@ impl VM {
         use Opcode::*;
         let mut i = 0;
         //start at the main function
-        self.frames.push(Frame { sip: 0, func_index:self.funcs.len()-1, sp: 0 });
+        self.frames.push(Frame { sip: 0, func_index:0, sp: 0 });
         let mut reset_ip = false;
         let mut skip_increment = false;
         loop {
@@ -670,8 +483,8 @@ impl VM {
 }
 
 pub struct Chunk {
-    code: Vec<u8>,
-    constants: Vec<Value>,
+    pub code: Vec<u8>,
+    pub constants: Vec<Value>,
 }
 
 
